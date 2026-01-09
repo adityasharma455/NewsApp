47a51820efbaf21bd76f64103c6144156e89386f
Aditya Sharma <aditya.sharma.cseai.2023@miet.ac.in>
2026-01-09 12:15:38 +0530

    Use BuildConfig for API key in ApiService

--- app/src/main/java/com/example/newsapp/data/apiService/ApiService.kt
+++ app/src/main/java/com/example/newsapp/data/apiService/ApiService.kt
@@ -3,19 +3,20 @@ package com.example.newsapp.data.apiService
 import retrofit2.http.GET
 import com.example.newsapp.data.model.ApiResponse
 import retrofit2.http.Query
+import com.example.newsapp.BuildConfig
 
 interface ApiService {
     @GET("top-headlines")
 
     suspend fun getHeadLines (
         @Query("country") country: String = "us",
-        @Query("apiKey") apiKey: String = "7691d7968a57436ab44fc2c6e468b59b"
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
     ): ApiResponse
 
     @GET("everything")
     suspend fun getEverything(
         @Query("q") q: String = "us",
-        @Query("apiKey") apiKey: String = "7691d7968a57436ab44fc2c6e468b59b"
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
 
     ): ApiResponse
 }
